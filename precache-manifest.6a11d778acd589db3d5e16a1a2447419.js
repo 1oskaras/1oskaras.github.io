df6f3361daeb1d49f824de25650dc29d4bb181b0
1oskaras <oskaras.riauba@gmail.com>
2020-06-23 13:28:50 +0300

    Add files via upload

---
+++ precache-manifest.6a11d778acd589db3d5e16a1a2447419.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00db84a04c8f4500f24c128e6d899087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2a08df4cef9e895b23",
+    "url": "/static/css/main.63204671.chunk.css"
+  },
+  {
+    "revision": "46e85c9b917f7c2ff546",
+    "url": "/static/js/2.147b0b1e.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.147b0b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2a08df4cef9e895b23",
+    "url": "/static/js/main.27dcd148.chunk.js"
+  },
+  {
+    "revision": "823c60cacc0c0cc80623",
+    "url": "/static/js/runtime-main.49765642.js"
+  },
+  {
+    "revision": "7e26ccadf4d6de93c45225ce174c9de3",
+    "url": "/static/media/WPf.7e26ccad.png"
+  },
+  {
+    "revision": "2168987aec8b0105221f9908e3b384a5",
+    "url": "/static/media/bg-un.2168987a.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "be6247cf79b04a194c33732fc4093395",
+    "url": "/static/media/oskaras.be6247cf.jpg"
+  }
+]);
